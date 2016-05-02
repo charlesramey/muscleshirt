46667c68a5aac5b4d1fef51ca7980e9a9275abde
Naveen Chandran <naveenkchandran@gmail.com>
2016-05-02 15:18:59 -0400

    Added preprocessing to smooth data using savitzky golay filter

--- data_analysis/determiner.py
+++ data_analysis/determiner.py
@@ -1,44 +1,86 @@
 import sys
 import json
+import scipy.signal as sig
 
 class Determiner(object):
 
-	"""
-	Sample file should point to a json list file that contains the keys corresponding to sensor values, plus the values in a single array for the time capture period. 
-	"""
-	def __init__(self, json_list_file):
-		self.json_list_file = json_list_file
+    """
+    Sample file should point to a json list file that contains the keys corresponding to sensor values, plus the values in a single array for the time capture period. 
+    """
+    def __init__(self, json_list_file):
+        self.json_file = json_list_file
+        self.smoothed_data = None
 
-		with open(self.json_list_file) as f:
-			self.values_dict = json.load(f)
+        with open(self.json_file) as f:
+            self.values_dict = json.load(f)
 
-
-	def determine(self):
-		pass
-
+        self.num_features = 5
 
 
-	def findEMGActivationLevels(self):
-		keys = ['emg_chest', 'emg_upper_arm', 'emg_forearm']
-		MAX_VALUE_THRESHOLD = .5
 
-		num_max_vals = {}
-		for k in keys:
-			emg_arr = self.values_dict[k]
-			max_values_present = [x for x in emg_arr if x >= max(emg_arr)*MAX_VALUE_THRESHOLD]
-			num_max_vals[k] = len(max_values_present)
+    def get_smoothed_data(self):
+        if self.smoothed_data: return self.smoothed_data
 
+        self.smoothed_data = {}
+        for key in self.values_dict:
+            self.smoothed_data[key] = self._smooth(self.values_dict[key])
 
-		return num_max_vals
+        return self.smoothed_data
 
 
+
+
+    def _smooth(self, list):
+        window_length = 11     #!!!!must be odd!!!!
+        poly_order = 7    #Polynomial order of the data
+
+        return sig.savgol_filter(list, window_length, poly_order)
+        
+
+
+
+    def determine(self, num_values):
+    	pass
+
+
+
+    def findEMGActivationLevels(self):
+    	keys = ['emg_chest', 'emg_upper_arm', 'emg_forearm']
+    	MAX_VALUE_THRESHOLD = .5
+
+    	num_max_vals = {}
+    	for k in keys:
+    		emg_arr = self.values_dict[k]
+    		max_values_present = [x for x in emg_arr if x >= max(emg_arr) * MAX_VALUE_THRESHOLD]
+    		num_max_vals[k] = len(max_values_present)
+
+
+    	return num_max_vals
+
+
+
+
+
+def main():
+    import sys, os
+    import matplotlib.pyplot as plt
+
+    json_file = "./training data/parsed/1_3_list.json"
+    fname = json_file.split(".json")[0].split("/")[-1]
+    d = Determiner(json_file)
+
+    smoothed_data = d.get_smoothed_data()
+
+    for i, k in enumerate(d.values_dict):
+        plt.plot(d.values_dict[k], color="b")
+        plt.plot(smoothed_data[k], color="r")
+        plt.title(fname + ": " + k)
+        plt.savefig("./figures/"+ fname + "-" + k + ".png")
+        plt.close("all")
+
+    print d.findEMGActivationLevels()
 
 
 if __name__ == "__main__":
-	import sys
-
-	json_file = sys.argv[1]
-	d = Determiner(json_file)
-	print d.findEMGActivationLevels()
-	
+    main()
 
